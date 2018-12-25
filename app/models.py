f9d704ccc5448b88379bbc11f607a46cb482c496
gambrinus304 <devilhunter2009@mail.ru>
2018-12-25 03:36:13 +0300

    added migrations, tags

--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ def slugify(s):
     return re.sub(pattern, '-', s)
 
 
+post_tags = db.Table('post_tags',
+                    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,6 +28,9 @@ class Post(db.Model):
         self.generate_slug()
 
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
@@ -29,3 +38,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {}, name: {}'.format(self.id, self.name)
