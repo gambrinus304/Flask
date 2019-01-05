bc609e6466e77832f124de3ece6e64c5e4280c04
gambrinus304 <devilhunter2009@mail.ru>
2019-01-05 04:11:34 +0300

    Added security, admin access, fix create tags from the admin

--- app/config.py
+++ app/config.py
@@ -3,3 +3,7 @@ class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:143@localhost/test1'
     SECRET_KEY = 'very secret, naprimer'
+
+    #Flask-security
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
--- app/models.py
+++ app/models.py
@@ -47,9 +47,13 @@ class Tag(db.Model):
     name = db.Column(db.String(100))
     slug = db.Column(db.String(100))
 
-    def __init__(self, *args, **kwargs):
+    def __irnit__(self, *args, **kwargs):
         super(Tag, self).__init__(*args, **kwargs)
         self.slug = slugify(self.name)
+
+    def generate_slug(self):
+            if self.name:
+                self.slug = slugify(self.name)
 
     def __repr__(self):
         return '{}'.format(self.name)
